b36630411c9e44516a46a1604326349b22f18201
Genocity <36272410+Genocity@users.noreply.github.com>
2018-09-01 23:45:06 +0300

    Modal window
    Animation "In"

--- scripts/main.js
+++ scripts/main.js
@@ -4,21 +4,34 @@ var doc = document;
 
 /*global variables*/
 
-
 document.getElementById('proceed').onclick = main;
 
 function main() {
+
 	var inputs = document.getElementsByTagName('input'),
 		input_one_val = inputs[0].value,
-		input_two_val = inputs[1].value;
+		input_two_val = inputs[1].value,
+		buttonOk = doc.getElementsByClassName("button-ok")[0],
+		wrapper = doc.getElementsByClassName("modal-wrapper")[0],
+		content = doc.getElementsByClassName("modal-content")[0],
+		header = doc.getElementsByTagName('h2')[0];
 
 	if ((input_one_val == "") || (input_two_val == "")) { //if inputs are empty
-		alert("Enter the value!");
+		header.innerText = "Enter the value!";
 	} else {
 		if (input_one_val == input_two_val) { //compare the values
-			alert("These Strings are the same");
+			header.innerText = "These Strings are the same";
 		} else {
-			alert("These Strings are different");
+			header.innerText = "These Strings are different";
 		}
+	}
+
+	show_hide_modal();
+
+	buttonOk.onclick = show_hide_modal;
+
+	function show_hide_modal() {
+		wrapper.classList.toggle("hidden");
+		content.classList.toggle("zoomInLeft");
 	}
 }
